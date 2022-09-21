58910278516688c26cacace45432372557e4c481
rzamfiriu <rzamfiriu@barracuda.com>
2022-09-21 20:18:40 +0300

    part2-2 example

--- part2/rendering/src/App.js
+++ part2/rendering/src/App.js
@@ -1,7 +1,28 @@
 import Note from "./components/Note"
+import {useState} from  'react'
 
 const App = (props) => {
-  const { notes } = props
+  const [notes, setNotes] = useState(props.notes)
+  const [newNote, setNewNote] = useState(
+    'a new note...'
+  ) 
+
+  const addNote = (event) => {
+    event.preventDefault()
+    const noteObject = {
+      content: newNote,
+      date: new Date().toISOString(),
+      important: Math.random() < 0.5,
+      id: notes.length + 1,
+    }
+  
+    setNotes(notes.concat(noteObject))
+    setNewNote('')
+  }
+  const handleNoteChange = (event) => {
+    console.log(event.target.value)
+    setNewNote(event.target.value)
+  }
 
   return (
     <div>
@@ -9,6 +30,11 @@ const App = (props) => {
       <ul>
           {notes.map(note => <Note key={note.id} note={note}/> )}
       </ul>
+      <form onSubmit={addNote}>
+        <input value={newNote} onChange={handleNoteChange} />
+        <button type="submit">save</button>
+        
+      </form> 
     </div>
   )
 }
