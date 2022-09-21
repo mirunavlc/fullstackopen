61e1fb710931c033aa986ef05989c4226b550339
rzamfiriu <rzamfiriu@barracuda.com>
2022-09-21 20:26:31 +0300

    part2-3 example from part2

--- part2/rendering/src/App.js
+++ part2/rendering/src/App.js
@@ -6,6 +6,11 @@ const App = (props) => {
   const [newNote, setNewNote] = useState(
     'a new note...'
   ) 
+  const [showAll, setShowAll] = useState(true)
+
+  const notesToShow = showAll
+  ? notes
+  : notes.filter(note => note.important === true)
 
   const addNote = (event) => {
     event.preventDefault()
@@ -27,8 +32,14 @@ const App = (props) => {
   return (
     <div>
       <h1>Notes</h1>
+      <div>
+        <button onClick={() => setShowAll(!showAll)}>
+          show {showAll ? 'important' : 'all' }
+        </button>
+      </div>
       <ul>
-          {notes.map(note => <Note key={note.id} note={note}/> )}
+
+          {notesToShow.map(note => <Note key={note.id} note={note}/> )}
       </ul>
       <form onSubmit={addNote}>
         <input value={newNote} onChange={handleNoteChange} />
